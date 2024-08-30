b57081314c95b60d81fba5998866b8a8cff3940d
Joao Pedro <msjoao.pedro@hotmail.com>
2024-08-30 18:12:55 -0300

    update

--- script.js
+++ script.js
@@ -80,45 +80,59 @@ window.addEventListener('load', function () {
 
 // function to show my love messages ------>
 
-const messages = [
-    "I wanna show you up!", 
-    "You got me stuck on the thought of you!", 
-    "You're more than the sunshine in my eyes", 
-    "You got those pretty eyes in your head, you know it", 
-    "You are exactly what I want",
-    "Sometimes, all I think about is you",
-    "Every time you come around, you know I can't say no",
-    "I never kissed a mouth that tastes like yours",
-    "Antes da gente dizer tchau, tem tanta coisa que eu quero te falar, como num filme, o que é bom faz o final",
-    "Se for por você, eu corro atrás dentro de um aeroporto",
-    "Talvez eu goste ainda mais de nós num dia inteiro colados a sós com tempo pra se discorrer a dois, sentir sua pele, sem tua roupa, não ligo se faz frio ou faz calor, hoje quem me ligar digo que tô nas nuvens de um poeta ou algo assim",
-    "Você é interestelar, nem a estrela mais linda tem a luz que cê tem",
-    "Nosso primeiro beijo",
-    "Eu só te achei a cara do pra sempre que eu sempre procurei",
-    "Fala pro seu passado que seu futuro é comigo",
-    "Eu, você e uma casinha, geladeira cheia, dormir de conchinha depois de umas brincadeiras",
-    "I wanna show you off",
-    "Nem o sol consegue me aquecer num dia ensolarado como você consegue aquecer meu coração só de sorrir para mim.",
-    "I love you",
-    "Você é incrível",
-    "Você é Maravilhosa",
-    "Quando você sorri eu me derreto",
-    "Amo tanto do seu beijo, seu toque, tudo que vem de você",
-    "Te ver e não te querer é impossível",
-    "Se você pudesse enxergar como eu te enxergo, você se apaixonaria toda vez que olhasse no espelho",
-    "Tudo começou quando você sorriu para mim, meu mundo acabou e começou naquele instante",
-    "Quero te fazer feliz enquanto eu respirar",
-    "Você me faz tão bem",
-    "Estou com saudade",
-    "VOCÊ PARECE ESCULPIDA POR ANJOS DE TÃO PERFEITA",
-    "Queria estar abraçado em você agora!",
-]
 
 function showMessage () {
-    let msg = Math.floor(Math.random() * messages.length);
+
+    const messages = [
+        "I wanna show you up!", 
+        "You got me stuck on the thought of you!", 
+        "You're more than the sunshine in my eyes", 
+        "You got those pretty eyes in your head, you know it", 
+        "You are exactly what I want",
+        "Sometimes, all I think about is you",
+        "Every time you come around, you know I can't say no",
+        "I never kissed a mouth that tastes like yours",
+        "Antes da gente dizer tchau, tem tanta coisa que eu quero te falar, como num filme, o que é bom faz o final",
+        "Se for por você, eu corro atrás dentro de um aeroporto",
+        "Talvez eu goste ainda mais de nós num dia inteiro colados a sós com tempo pra se discorrer a dois, sentir sua pele, sem tua roupa, não ligo se faz frio ou faz calor, hoje quem me ligar digo que tô nas nuvens de um poeta ou algo assim",
+        "Você é interestelar, nem a estrela mais linda tem a luz que cê tem",
+        "Nosso primeiro beijo",
+        "Eu só te achei a cara do pra sempre que eu sempre procurei",
+        "Fala pro seu passado que seu futuro é comigo",
+        "Eu, você e uma casinha, geladeira cheia, dormir de conchinha depois de umas brincadeiras",
+        "I wanna show you off",
+        "Nem o sol consegue me aquecer num dia ensolarado como você consegue aquecer meu coração só de sorrir para mim.",
+        "I love you",
+        "Você é incrível",
+        "Você é Maravilhosa",
+        "Quando você sorri eu me derreto",
+        "Amo tanto do seu beijo, seu toque, tudo que vem de você",
+        "Te ver e não te querer é impossível",
+        "Se você pudesse se enxergar como eu te enxergo, você se apaixonaria toda vez que olhasse no espelho",
+        "Tudo começou quando você sorriu para mim, meu mundo acabou e começou naquele instante",
+        "Quero te fazer feliz enquanto eu respirar",
+        "Você me faz tão bem",
+        "Estou com saudade",
+        "VOCÊ PARECE ESCULPIDA POR ANJOS DE TÃO PERFEITA",
+        "Queria estar abraçado em você agora!",
+        "Cansei, te quero todinha pra mim, talvez, ajoelhar pra ouvir seu sim",
+        "Me apaixonei, mais uma vez, mais uma vez fui pelo cano",
+        "Prefiro ter você assim, bem pertinho de mim, de manhã, de madrugada, te quero também de tarde, quando nóis fica junto somos inimigo do fim",
+        "Give your all to me, I'ill give my all to you",
+        "I see my future in your eyes",
+        "When I see your face, thre's not a thing that I would change and when you smile the whole world stops and stares for a while",
+        "You are my paradise",
+        "Pensar em você, é pensar no meu futuro",
+        "Queria te dar todas as palavras bonitas que existem no mundo, mas não acho que exista alguma que, sozinha, defina a sua beleza",
+        "Quando estou perto de você, eu não preciso de mais nada",
+        "Je t'aime"    
+    ]
+
     for (let i = 0; i <= messages.length; i++) {
+        let msg = Math.floor(Math.random() * messages.length);
         if (i = msg) {
             return messages[i];
+
         } else {
             return "I love you";
         }
@@ -128,3 +142,30 @@ function showMessage () {
 window.addEventListener('load', function () {
     document.getElementById("message-text").innerHTML = showMessage()
 })
+
+
+
+// let write = str => {
+//     let arrFromStr = str.split('');
+//     let l = 0;
+//     let printStr = setInterval(function() {
+
+//         if(arrFromStr[l] === ' ') {
+//             document.getElementById("message-text").innerHTML += arrFromStr[l];
+//             document.getElementById("message-text").innerHTML += arrFromStr[l + 1];
+//             l += 2;
+//         } else {
+//             document.getElementById("message-text").innerHTML += arrFromStr[l];
+//             l++;
+//         }
+//         if (l === arrFromStr.length) {
+//             clearInterval(printStr);
+//             document.getElementById("message-text").style.transition = '0.3s ease';
+//             // document.body.style.fontSize = '150px';
+//             // document.body.style.color = '#000000';
+//             // document.body.style.background = '#ffffff';
+//         }
+
+//     }, 50);
+// };
+// return write(messages[i]);
